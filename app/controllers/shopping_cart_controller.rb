ad54c1e0acd10684c546421e261d975045b3d3bc
johan <johan.schantz@telia.com>
2017-03-15 15:07:07 +0100

    added image to shopping cart

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,17 +1,15 @@
 class ShoppingCartController < ApplicationController
 
-  def add_to_cart
-  end
+    def add_to_cart
 
-    def create
-    if cookies['cart_id']
-    else
-       @cart = ShoppingCart.find_or_create_by(id: session[:cart_id])
-      # session[:cart_id] = @cart.id
-    end
+      if cookies['cart_id']
+        @cart = ShoppingCart.find_or_create_by(id: session[:cart_id])
+        # session[:cart_id] = @cart.id
+      else
+        @cart = ShoppingCart.create
+      end
 
-      @cart = Cart.create
-      @image = Image.find(params[:id])
+      @image = Image.find(params[:image_id])
       @cart.add(@image, @image.price)
     end
   end
