df37dc3b8538f63b9aa5ddbe9cf16a6d04f8be89
tochman <thomas@craftacademy.se>
2017-03-16 08:20:21 +0100

    moves #create_cart to application_controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,13 +1,9 @@
 class ImagesController < ApplicationController
-  before_action :create_cart, only: :index
+
 
   def index
     @images = Image.all
   end
 
-  private
 
-  def create_cart
-    session[:cart_id] = ShoppingCart.create.id unless session[:cart_id]
-  end
 end
