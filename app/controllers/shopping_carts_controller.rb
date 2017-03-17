abb4db01b21436343b835dfd284f7259165fc990
felixottander <felix.ottander@yahoo.com>
2017-03-17 09:24:10 +0100

    Changed the way we calculated the total price.

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -8,12 +8,6 @@ class ShoppingCartsController < ApplicationController
   private
 
   def get_shopping_cart
-    @sum = 0
     @cart = ShoppingCart.find(session[:cart_id])
-
-    @cart.shopping_cart_items.each do |total|
-      @sum += total.item.price
-    end
-
   end
 end
